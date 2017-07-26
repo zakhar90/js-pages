e6a7ad179e4adb8fec367dfb32b32e60b8c22396
Zakharii <zakhar90@gmail.com>
2017-07-26 14:10:33 +0000

    clear js

--- js/index.js
+++ js/index.js
@@ -1,30 +1,3 @@
-// function add(a) {
-//     let result = Math.sqrt(a);
-//     return result;
-// }
-
-// let a = 64;
-// let x = add(a);
-// document.write(x);    
-
-
-
-// function add(a, b) {
-//     let result = Math.sqrt(b / a);
-//     return result;
-// }
-
-// let a = 4;
-// let b = 16;
-// let x = add(a, b);
-// document.write(x);
-
-
-
-// A = 1 B = 2 C = 1    X1= -1
-
-
-
 function calcDiscr(a, b, c) {
     return b * b - 4 * a * c;
 }
@@ -34,13 +7,15 @@ function solveQuadratic(a, b, c) {
     let discr = calcDiscr(a, b, c);
     if (discr < 0) {
         return 'No roots';
-    }  else if (discr > 0) {
+    }
+    else if (discr > 0) {
         x1 = (-b + Math.sqrt(discr)) / (2 * a);
         x2 = (-b - Math.sqrt(discr)) / (2 * a);
-    return 'x1 = ' + x1 + ', x2= ' + x2;
-    } else if (discr === 0) {
+        return 'x1 = ' + x1 + ', x2= ' + x2;
+    }
+    else if (discr === 0) {
         x1 = -b / (2 * a);
-    return 'x1 = ' + x1;
+        return 'x1 = ' + x1;
     }
 }
 
@@ -51,35 +26,3 @@ var c = parseFloat(prompt('Enter C'));
 var result = solveQuadratic(a, b, c);
 
 document.write(result);
-
-
-
-
-// A = -2 B = 3 C = 2    X1= -0.5   X2= 2
-
-
-
-// function calcDiscr(a, b, c) {
-//     return b * b - 4 * a * c;
-// }
-
-// function solveQuadratic(a, b, c) {
-//     let x1, x2;
-//     let discr = calcDiscr(a, b, c);
-//     if (discr < 0) {
-//         return 'No roots';
-//     }  else if (discr > 0) {
-//         x1 = (-b + Math.sqrt(discr)) / (2 * a);
-//         x2 = (-b - Math.sqrt(discr)) / (2 * a);
-//     }
-    
-//     return 'x1 = ' + x1 + ', x2= ' + x2;
-// }
-
-// var a = parseFloat(prompt('Enter A'));
-// var b = parseFloat(prompt('Enter B'));
-// var c = parseFloat(prompt('Enter C'));
-
-// var result = solveQuadratic(a, b, c);
-
-// document.write(result);
